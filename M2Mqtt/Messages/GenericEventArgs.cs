7018c9b71b0db6da3e153ddb988e19b455affd1e
Ross Bevington <ross.bevington@microsoft.com>
2020-08-18 13:08:51 +0100

    Fixes some issues in the .NET standard build

--- M2Mqtt/Messages/GenericEventArgs.cs
+++ M2Mqtt/Messages/GenericEventArgs.cs
@@ -13,26 +13,26 @@ namespace uPLibrary.Networking.M2Mqtt.Messages
         public GenericEventArgs(Events.Events.Options eventType, EventArgs EA) {
 
             Dict = new Dictionary<string, object>();
-#if (!MONO)
+//#if (!MONO)
 
-            // I don't like this, but it's handy
-            foreach (var p in EA.GetType().GetProperties()) {
-                var t = p.GetValue(EA);
-                dynamic q = t;
-                if (t.GetType() == typeof(byte))
-                {
-                    int s = (int)q;
-                    q = $"{s}";
-                }
-                else if (t.GetType() == typeof(byte[]))
-                {
-                    // Maybe represent as hex instead??..
-                    q = Encoding.ASCII.GetString((byte[])q);
-                }
-                this.Dict.Add(p.Name, q);
-            }
-            Dict.Add("EventType", eventType.ToString());
-#endif
+//            // I don't like this, but it's handy
+//            foreach (var p in EA.GetType().GetProperties()) {
+//                var t = p.GetValue(EA);
+//                dynamic q = t;
+//                if (t.GetType() == typeof(byte))
+//                {
+//                    int s = (int)q;
+//                    q = $"{s}";
+//                }
+//                else if (t.GetType() == typeof(byte[]))
+//                {
+//                    // Maybe represent as hex instead??..
+//                    q = Encoding.ASCII.GetString((byte[])q);
+//                }
+//                this.Dict.Add(p.Name, q);
+//            }
+//            Dict.Add("EventType", eventType.ToString());
+//#endif
         }
     }
 }
